14e904b7aae72236ec7e368a2e031055bfe17c23
Jack Schultz <jackschultz23@gmail.com>
2016-12-08 16:19:18 -0600

    no asin initializer

--- config/initializers/asin.rb
+++
@@ -1,5 +0,0 @@
-ASIN::Configuration.configure do |config|
-  config.secret        = 'Ks287HiL8p6ZNLM+GFRu/tuG9BJylOzOPHI9yDwj'
-  config.key           = 'AKIAIHTT6ORMUADXEIQA'
-  config.associate_tag = 'pmentions-20'
-end
