b2c46c810c913781ab7b3ceb7d746b5a80d64240
Jack Schultz <jackschultz23@gmail.com>
2016-12-08 16:14:47 -0600

    removing ability to run perform on job

--- app/jobs/scraping_job.rb
+++ app/jobs/scraping_job.rb
@@ -8,9 +8,9 @@ class ScrapingJob < ActiveJob::Base
     #subsite = Subsite.find(site_id)
     #class_name = "Scrapers::#{subsite.site.name.tr(" ", '')}Scraper".constantize
     #scraper = class_name.new(subsite)
-    scraper = Scrapers::RedditScraper.new
+    #scraper = Scrapers::RedditScraper.new
     #scraper.gather_comments
-    scraper.gather_threads_search
+    #scraper.gather_threads_search
     #scraper.gather_threads
   end
 
