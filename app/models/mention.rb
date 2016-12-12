c68d2200f351dac8a32cc196bac9e455d8a713a1
Jack Schultz <jackschultz23@gmail.com>
2016-12-12 15:29:25 -0600

    more view updating

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,7 +1,7 @@
 class Mention < ApplicationRecord
 
-  belongs_to :product
-  belongs_to :comment
+  belongs_to :product, counter_cache: true
+  belongs_to :comment, counter_cache: true
 
   self.per_page = 20
 
