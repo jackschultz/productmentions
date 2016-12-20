bc24794032308919a886e5c66a0aa2145f1bea9f
Jack Schultz <jackschultz23@gmail.com>
2016-12-20 17:01:05 -0600

    adding /products to see most mentioned overall

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,27 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
+    interval = interval_from_params
+    @products = Product.joins(:mentions).where("mentions.created_at > current_date - interval '#{interval}'").select('products.*, count(product_id) as "mentions_count"').group("products.id").order("mentions_count desc").paginate(:page => params[:page])
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+
+  def interval_from_params
+    if params[:tf] == 'day'
+      interval = '1 days'
+    elsif params[:tf] == 'week'
+      interval = '1 week'
+    elsif params[:tf] == 'month'
+      interval = '30 days'
+    else
+      interval = '1 week'
+    end
+    return interval
   end
 
 end
