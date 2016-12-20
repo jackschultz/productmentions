47cd79053baaebd75f6c9cec0daed899ff6b0de9
Jack Schultz <jackschultz23@gmail.com>
2016-12-20 11:08:35 -0600

    homepage updates

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,10 @@
 class HomesController < ApplicationController
 
   def index
-    @mentions = Mention.joins(:comment).order("comments.written_at desc").limit(5)
-    @product_groups = ProductGroup.joins(:mentions).select('product_groups.*, count(product_id) as "mentions_count"').group("product_groups.id").order("mentions_count desc").limit(5)
-    @subsites = Subsite.joins(:mentions).select('subsites.*, count(comment_id) as "mentions_count"').group("subsites.id").order("mentions_count desc").limit(5)
+    interval = '1 week'
+    @mentions = Mention.joins(:comment).order("comments.written_at desc").limit(20)
+    @product_groups = ProductGroup.joins(:mentions).where("mentions.created_at > current_date - interval '#{interval}'").select('product_groups.*, count(product_id) as "mentions_count"').group("product_groups.id").order("mentions_count desc").limit(5)
+    @subsites = Subsite.joins(:mentions).where("mentions.created_at > current_date - interval '#{interval}'").select('subsites.*, count(comment_id) as "mentions_count"').group("subsites.id").order("mentions_count desc").limit(5)
   end
 
 end
